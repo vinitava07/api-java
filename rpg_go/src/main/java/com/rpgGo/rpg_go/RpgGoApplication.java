ba6b8c9c0134c99f714ff4a0e224f3f68123a416
= <vinitava07@gmail.com>
2024-04-12 12:24:07 -0300

    first commit

---
+++ rpg_go/src/main/java/com/rpgGo/rpg_go/RpgGoApplication.java
@@ -0,0 +1,14 @@
+package com.rpgGo.rpg_go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgGoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(RpgGoApplication.class, args);
+	}
+
+}
---
+++ rpg_go/src/test/java/com/rpgGo/rpg_go/RpgGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpgGo.rpg_go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
